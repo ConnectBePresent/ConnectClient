6267c521938dcf70c6e238a7fad2d5a5bf51e110
VishnuSanal <t.v.s10123@gmail.com>
2024-04-16 14:19:34 +0530

    update welcome screen

--- app/src/main/java/com/example/connectcompose/Welcome.kt
+++ app/src/main/java/com/example/connectcompose/Welcome.kt
@@ -1,14 +1,21 @@
 package com.example.connectcompose
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -16,91 +23,55 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Welcome(modifier : Modifier,
-            navController: NavController){
+fun Welcome(
+    modifier: Modifier, navController: NavController
+) {
+    Surface(Modifier.fillMaxSize()) {
 
+        val name = remember { mutableStateOf("") }
 
-    val name = remember {
-
-        mutableStateOf("Guest")
-    }
-
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth(1f)
-            .padding(20.dp),
-        verticalArrangement = Arrangement.spacedBy(25.dp)
-    ){
-
-        Row {
-             Text("Hi,\n\n\nWELCOME TO\n\n\nCONNECT",
-                 fontSize = 50.sp,
-                 color = Color.White,
-                 modifier = Modifier
-                     )
-
-        }
-        Row {
-            Text(text = "What should we call you",
-                fontSize = 20.sp,
-                color = Color.White)
-
-        }
-        Row(modifier = Modifier
-            .fillMaxWidth(1f)
-            .padding(10.dp)
-
-        ) {
-
-            TextField(value = name.value, onValueChange = {
-                 name.value = it
-            },
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    ,
-                shape = RoundedCornerShape(30.dp),
-                colors = TextFieldDefaults.textFieldColors(
-                    
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
-                )
-                )
-
-
-
-        }
-        Spacer(modifier = Modifier
-            .weight(1f))
-        Row (
+        Column(
             modifier = Modifier
-                .fillMaxWidth(1f)
-                .padding(horizontal = 10.dp),
-            horizontalArrangement = Arrangement.End
+                .wrapContentSize()
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.background)
+                .padding(16.dp),
+        ) {
+            Text(
+                "Welcome to Connect!",
+                Modifier.padding(16.dp, 16.dp, 16.dp, 0.dp),
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+            Text(
+                text = "What should we call you?",
+                Modifier.padding(16.dp, 16.dp, 16.dp, 0.dp),
+                fontSize = 20.sp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
 
+            OutlinedTextField(
+                modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 0.dp),
+                shape = RoundedCornerShape(16.dp),
+                label = { Text("Name") },
+                value = name.value,
+                onValueChange = {
+                    name.value = it
+                },
+                keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words)
+            )
 
-        ){
-              Button(onClick = {
-                  navController.navigate(route = Screen.StudentEntry.route
-                  )
-              }) {
-                  Text("Next")
-
-              }
+            Button(modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 0.dp), onClick = {
+                navController.navigate(route = Screen.StudentEntry.route)
+            }) { Text("Next") }
         }
-
-
-
-
     }
-
-
-
 }
